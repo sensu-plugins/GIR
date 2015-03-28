bca0dddefc04dd31a87c5f8cb423b546e7d81f0a
Matt Jones <mattjones@yieldbot.com>
2015-03-28 01:00:55 -0400

    update

--- roles/cent_base.rb
+++ roles/cent_base.rb
@@ -1,7 +1,7 @@
 name "cent_base"
 description "A base role to configure a standard CentOS development environment"
 run_list 'recipe[yum]',
-        #  'recipe[yum-epel]',
-        #  'recipe[yum-centos]',
-        #  'recipe[yum-repoforge]',
+         'recipe[yum-epel]',
+         'recipe[yum-centos]',
+         'recipe[yum-repoforge]',
          'recipe[plugins-dev]'
